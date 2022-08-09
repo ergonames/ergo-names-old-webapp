341aab6dddd0d5013189b3bbe512f5a8bf43d879
Zack Balbin <zack@zackbalbin.com>
2022-08-08 20:06:18 -0700

    use custom tx module

--- src/views/MintNFT.vue
+++ src/views/MintNFT.vue
@@ -39,8 +39,8 @@
         Please try again later.
       </p>
       <b
-        >To use the "connect wallet" feature, please install Nautilus Testnet wallet and
-        connect your ERG wallet there.</b
+        >To use the "connect wallet" feature, please install Nautilus Testnet
+        wallet and connect your ERG wallet there.</b
       >
     </div>
   </div>
@@ -48,214 +48,19 @@
 
 <script>
 import { mapState } from 'vuex'
-import JSONBigInt from 'json-bigint'
 
-import getCurrentChainHeight from '../utils/explorer'
+const ergonamesTxLib = import('ergonames-tx-lib')
 
-const wasm = import('ergo-lib-wasm-browser')
+// function displayTxId(txId) {
+//   // eslint-disable-next-line
+//   alert(
+//     `<a href="https://testnet.ergoplatform.com/en/transactions/${txId}" target='_blank'> Transaction on the explorer</a>`
+//   )
+// }
 
-async function getUtxos(amountToSend) {
-  const fee = BigInt((await wasm).TxBuilder.SUGGESTED_TX_FEE().as_i64().to_str())
-  const fullAmountToSend = BigInt(1000) * amountToSend + fee
-  // eslint-disable-next-line
-  const utxos = await ergo.get_utxos(fullAmountToSend.toString())
-  const filteredUtxos = []
-  /* eslint-disable no-await-in-loop */
-  utxos.forEach(async (utxo) => {
-    try {
-      await (await wasm).ErgoBox.from_json(JSONBigInt.stringify(utxo))
-      filteredUtxos.push(utxo)
-    } catch (e) {
-      console.error('[getUtxos] UTxO failed parsing:', utxo, e)
-    }
-  })
-  return filteredUtxos
-}
-
-async function signTx(txToBeSigned) {
-  try {
-    // eslint-disable-next-line
-    return await ergo.sign_tx(txToBeSigned)
-  } catch (err) {
-    const msg = `[signTx] Error: ${JSON.stringify(err)}`
-    console.error(msg, err)
-    // eslint-disable-next-line
-    alert(msg)
-    return null
-  }
-}
-
-async function submitTx(txToBeSubmitted) {
-  try {
-    // eslint-disable-next-line
-    return await ergo.submit_tx(txToBeSubmitted)
-  } catch (err) {
-    const msg = `[submitTx] Error: ${JSON.stringify(err)}`
-    console.error(msg, err)
-    // eslint-disable-next-line
-    alert(msg)
-    return null
-  }
-}
-
-async function processTx(txToBeProcessed) {
-  const msg = (s) => {
-    console.log('[processTx]', s)
-    // eslint-disable-next-line
-    alert(s)
-  }
-  const signedTx = await signTx(txToBeProcessed)
-  if (!signedTx) {
-    console.log(`No signed tx`)
-    return null
-  }
-  msg('Transaction signed - awaiting submission')
-  const txId = await submitTx(signedTx)
-  if (!txId) {
-    console.log(`No submitted tx ID`)
-    return null
-  }
-  msg('Transaction submitted - thank you for your donation!')
-  return txId
-}
-
-function displayTxId(txId) {
-  // eslint-disable-next-line
-  alert(
-    `<a href="https://testnet.ergoplatform.com/en/transactions/${txId}" target='_blank'> Transaction on the explorer</a>`
-  )
-}
-
-function parseUTXO(json) {
-  const newJson = { ...json }
-  if (newJson.assets === null) {
-    newJson.assets = []
-  }
-  return {
-    boxId: newJson.boxId,
-    value: newJson.value.toString(),
-    ergoTree: newJson.ergoTree,
-    assets: newJson.assets.map((asset) => ({
-      tokenId: asset.tokenId,
-      amount: asset.amount.toString(),
-    })),
-    additionalRegisters: newJson.additionalRegisters,
-    creationHeight: newJson.creationHeight,
-    transactionId: newJson.transactionId,
-    index: newJson.index,
-  }
-}
-
-function parseJson(str) {
-  const json = JSONBigInt.parse(str)
-  return {
-    id: json.id,
-    inputs: json.inputs,
-    dataInputs: json.dataInputs,
-    outputs: json.outputs.map((output) => parseUTXO(output)),
-  }
-}
-
-async function sendErgoTransaction(amount, sendTo) {
-  console.log(`amount to send: ${amount}`)
-  console.log(`send to: ${sendTo}`)
-  // eslint-disable-next-line
-  alert('Creating transaction')
-  const creationHeight = await getCurrentChainHeight()
-  console.log(`current height: ${creationHeight}`)
-  const amountToSend = BigInt(amount)
-  const amountToSendBoxValue = (await wasm).BoxValue.from_i64((await wasm).I64.from_str(amountToSend.toString()))
-  const utxos = await getUtxos(amountToSend)
-  const utxosValue = utxos.reduce(
-    (acc, utxo) => (acc += BigInt(utxo.value)),
-    BigInt(0)
-  )
-  console.log('utxos', utxosValue, utxos)
-
-  const changeValue =
-    utxosValue -
-    amountToSend -
-    BigInt((await wasm).TxBuilder.SUGGESTED_TX_FEE().as_i64().to_str())
-  console.log(`${changeValue} | cv.ts() = ${changeValue.toString()}`)
-  // const changeValueBoxValue = wasm.BoxValue.from_i64(
-  //   wasm.I64.from_str(changeValue.toString())
-  // )
-  // eslint-disable-next-line
-  const changeAddr = await ergo.get_change_address()
-  console.log(`changeAddr = ${JSON.stringify(changeAddr)}`)
-  const selector = new (await wasm).SimpleBoxSelector()
-  const boxSelection = selector.select(
-    (await wasm).ErgoBoxes.from_boxes_json(utxos),
-    (await wasm).BoxValue.from_i64(
-      amountToSendBoxValue
-        .as_i64()
-        .checked_add((await wasm).TxBuilder.SUGGESTED_TX_FEE().as_i64())
-    ),
-    new (await wasm).Tokens()
-  )
-  console.log(`boxes selected: ${boxSelection.boxes().len()}`)
-  const outputCandidates = (await wasm).ErgoBoxCandidates.empty()
-  const donationBoxBuilder = new (await wasm).ErgoBoxCandidateBuilder(
-    amountToSendBoxValue,
-    (await wasm).Contract.pay_to_address((await wasm).Address.from_base58(sendTo)),
-    creationHeight
-  )
-  try {
-    outputCandidates.add(donationBoxBuilder.build())
-  } catch (e) {
-    console.log(`building error: ${e}`)
-    throw e
-  }
-  // outputCandidates.add(changeBoxBuilder.build())
-  console.log(`utxosval: ${utxosValue.toString()}`)
-  const txBuilder = (await wasm).TxBuilder.new(
-    boxSelection,
-    outputCandidates,
-    creationHeight,
-    (await wasm).TxBuilder.SUGGESTED_TX_FEE(),
-    (await wasm).Address.from_base58(changeAddr),
-    (await wasm).BoxValue.SAFE_USER_MIN()
-  )
-  // changeValueBoxValue)
-  const dataInputs = new (await wasm).DataInputs()
-  // random tx we sent via the connector before - not referenced in any smart contract right now
-  // dataInputs.add(new wasm.DataInput(wasm.BoxId.from_str("0f0e4c71ccfbe7e749591ef2a906607b415deadee8c23a8d822517c4cd55374e")))
-  txBuilder.set_data_inputs(dataInputs)
-  const tx = parseJson(txBuilder.build().to_json())
-  console.log(`tx: ${JSONBigInt.stringify(tx)}`)
-  console.log(`original id: ${tx.id}`)
-  // sigma-rust doesn't support most compilation so manually insert it here
-  // this is HEIGHT > 1337 but in hex and without the checksum/etc for the address of the contract
-  // tx.outputs[0].ergoTree = "100104f214d191a37300"
-  // and this is a register-using one
-  // tx.outputs[0].ergoTree = "1002040004f2c001d193e4c6b2a573000004047301"
-  // and we rebuild it using
-  const correctTx = parseJson(
-    (await wasm).UnsignedTransaction.from_json(JSONBigInt.stringify(tx)).to_json()
-  )
-  console.log(`correct tx: ${JSONBigInt.stringify(correctTx)}`)
-  console.log(`new id: ${correctTx.id}`)
-  // we must use the exact order chosen as after 0.4.3 in sigma-rust
-  // this can change and might not use all the utxos as the coin selection
-  // might choose a more optimal amount
-  correctTx.inputs = correctTx.inputs.map((box) => {
-    console.log(`box: ${JSONBigInt.stringify(box)}`)
-    const fullBoxInfo = utxos.find((utxo) => utxo.boxId === box.boxId)
-    return {
-      ...fullBoxInfo,
-      extension: {},
-    }
-  })
-  // eslint-disable-next-line
-  alert('Awaiting transaction signing')
-  console.log(`${JSONBigInt.stringify(correctTx)}`)
-
-  processTx(correctTx).then((txId) => {
-    console.log('[txId]', txId)
-    if (txId) {
-      displayTxId(txId)
-    }
-  })
+async function send(amount) {
+  const lib = await ergonamesTxLib
+  await lib.send_transaction(amount)
 }
 
 export default {
@@ -292,15 +97,11 @@ export default {
       this.ergoNameAvailable = true
       this.ergoNameUnavailable = false
     },
-    mintNFT(event) {
+    async mintNFT(event) {
       event.preventDefault()
       // ================================= Send money ============================
-      const amountToBeSent = 1 * 1e9
-      const sendToAddress =
-        '3Wxf2LxF8HUSzfnT6bDGGUDNp1YMvWo5JWxjeSpszuV6w6UJGLSf'
-      sendErgoTransaction(amountToBeSent, sendToAddress).then((callback) => {
-        console.log(callback)
-      })
+      const amountToBeSent = 100000000
+      await send(amountToBeSent)
     },
     onReset(event) {
       event.preventDefault()
